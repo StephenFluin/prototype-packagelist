b2034455bfafa2627d8cb24b400dd787dbc17ef6
Stephen Fluin <stephen.fluin@gmail.com>
2025-11-13 22:10:30 -0600

    init: basic app scaffold

---
+++ src/app/view-ecosystem/view-ecosystem.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ecosystem',
+  imports: [],
+  template: `
+    <p>
+      view-ecosystem works!
+    </p>
+  `,
+  styles: ``,
+})
+export class ViewEcosystem {
+
+}
---
+++ src/app/view-package/view-package.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-package',
+  imports: [],
+  template: `
+    <p>
+      view-package works!
+    </p>
+  `,
+  styles: ``,
+})
+export class ViewPackage {
+
+}
